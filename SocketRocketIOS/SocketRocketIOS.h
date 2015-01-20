d4d43898ca7ed8587a2ccdf539e72e3fa53bb6f0
Danil Zvyagintsev <danil@autoservice61.ru>
2015-01-19 21:01:57 -0500

    added scheme and target for building dynamic framework

---
+++ SocketRocketIOS/SocketRocketIOS.h
@@ -0,0 +1,17 @@
+//
+//  SocketRocketIOS.h
+//  SocketRocketIOS
+//
+//  Created by Danil Zvyagintsev on 1/19/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocketRocketIOS.
+FOUNDATION_EXPORT double SocketRocketIOSVersionNumber;
+
+//! Project version string for SocketRocketIOS.
+FOUNDATION_EXPORT const unsigned char SocketRocketIOSVersionString[];
+
+#import "SRWebSocket.h"
